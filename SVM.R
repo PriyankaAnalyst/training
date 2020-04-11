857ba24974baa59e422ea72388b7090b4abb7d78
Priyanka Gaikwad <60464854+PriyankaAnalyst@users.noreply.github.com>
2020-04-11 22:05:39 +0530

    SVM.R
    Forest Fires and Salary Data

--- SVM.R
+++ SVM.R
@@ -1,27 +1,90 @@
 # classify the Size_Categorie using SVM, Size_Categorie the burned area of the forest ( Small , Large)
 
+
 library(kernlab)
 library(caret)
+library(plyr)
+library(e1071)
+
+forest_data <- read.csv(file.choose())
+
+str(forest_data)
+View(forest_data)
+
+# The area value has lots of zeros
+ 
+hist(forest_data$area)  #histrogram plot for area field, and checking the zeros count
+rug(forest_data$area)  #rug checking the zero count
+
+summary(forest_data)
 
 
-forest <- read.csv(choose.files())
+# Prediction of Forest fires requires only prediction from 
+# temperature, rain, relative humidity and wind speed
 
-# train and test analysis
+# Applying Normalization Method :
 
-fires_train<-forest[1:450,]
+normalize<-function(x){
+  return ( (x-min(x))/(max(x)-min(x)))
+}
+forest_data$temp = normalize(forest_data$temp)
+forest_data$RH   = normalize(forest_data$RH)
+forest_data$wind = normalize(forest_data$wind)
+forest_data$rain = normalize(forest_data$rain)
 
-fires_test<-forest[451:517,]
+attach(forest_data)
 
-model1<-ksvm(size_category ~.,data = fires_train,kernel = "vanilladot")
+#Train and Test dataset.Data Partitioning
+
+
+set.seed(123) #reproducing the results
+
+forest_train <- forest_data[1:400, ]
+forest_test <- forest_data[401:517, ]
+
+?kernel
+
+model1 <- ksvm(size_category~temp+rain+wind+RH, data = forest_data, kernel = "vanilladot")
 model1
 
-fire_pred<-predict(model1,fires_test)
-head(fire_pred)
-table(fire_pred,fires_test$size_category)
+area_pred <- predict(model1, forest_test)
 
-agreement<-fire_pred==fires_test$size_category
+table(area_pred,forest_test$size_category) #confusion matrix
+
+agreement <- area_pred == forest_test$size_category
 table(agreement)
-prop.table(table(agreement)) #98% is the accuracy.
+
+prop.table(table(agreement))
+
+# Different types of kernels 
+# "rbfdot", "polydot", "tanhdot", "vanilladot", "laplacedot", 
+# "besseldot", "anovadot", "splinedot", "matrix"
+
+# kernel = rbfdot 
+
+model_rbfdot <- ksvm(size_category~temp+rain+wind+RH, data = forest_test, kernel = "rbfdot")
+
+pred_rbfdot<-predict(model_rbfdot,newdata=forest_test)
+
+mean(pred_rbfdot==forest_test$size_category) # 74.35
+
+# kernal = besseldot
+
+model_besseldot <- ksvm(size_category~temp+rain+wind+RH, data = forest_test, kernel = "besseldot")
+
+pred_besseldot<-predict(model_besseldot,newdata=forest_test)
+
+mean(pred_besseldot==forest_test$size_category) # 71.79
+
+# kernel = polydot
+
+model_polydot <- ksvm(size_category~temp+rain+wind+RH, data = forest_train, kernel = "polydot")
+
+pred_polydot<-predict(model_polydot,newdata=forest_test)
+
+mean(pred_polydot==forest_test$size_category) # 70.94
+
+****************************************************
 
 ******************************************************************************
 
